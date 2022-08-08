b2c52ac06418adc86ff0f8cb9bbf330f3f348df2
tangpan360 <18321171167@163.com>
2022-08-08 17:23:15 +0800

    batch_size 设置为2，作为对比实验的 baseline

--- helper_tool.py
+++ helper_tool.py
@@ -75,7 +75,8 @@ class ConfigSemantic3D:
     num_classes = 8  # Number of valid classes
     sub_grid_size = 0.06  # preprocess_parameter
 
-    batch_size = 4  # batch_size during training
+    # batch_size = 4  # batch_size during training
+    batch_size = 2  # batch_size during training
     val_batch_size = 16  # batch_size during validation and test
     train_steps = 500  # Number of steps per epochs
     val_steps = 100  # Number of validation steps per epoch
